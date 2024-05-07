7dbd38e9618a902f7c81265b60624fae58426e1a
DongchenZhao <zdc2022@outlook.com>
2024-05-07 21:19:18 +0800

    RRC: Remove resource closes

--- src/main/java/com/example/mutantGen/MutatorType.java
+++ src/main/java/com/example/mutantGen/MutatorType.java
@@ -7,11 +7,8 @@ public enum MutatorType {
     RFET, // remove file existance throws
     RUL, // remove unlock
     RSB, // remove synchronized block
-
     UNE, // upcast network exception
-
-
-
+    RRC, // remove resource closes
 
     // RLUL, // remove lock and unlock
     // RINT, //TODO remove exception throw statements in if (Obj == NULL) statements

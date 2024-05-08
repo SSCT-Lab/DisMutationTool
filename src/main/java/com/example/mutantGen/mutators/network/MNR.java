9485c240dc11f502a5a215594ef4e4b0ce9680af
DongchenZhao <zdc2022@outlook.com>
2024-05-08 12:54:17 +0800

    MNR：modify network resource checks

---
+++ src/main/java/com/example/mutantGen/mutators/network/MNR.java
@@ -0,0 +1,92 @@
+package com.example.mutantGen.mutators.network;
+
+import com.example.mutantGen.Mutant;
+import com.example.mutantGen.MutantGen;
+import com.example.mutantGen.MutatorType;
+import com.example.mutantGen.mutators.RFB;
+import com.example.utils.FileUtil;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.BooleanLiteralExpr;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
+import com.github.javaparser.resolution.UnsolvedSymbolException;
+import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MNR extends MutantGen {
+
+    public static MutatorType mutator = MutatorType.MNR;
+
+    private static final Logger logger = LogManager.getLogger(MNR.class);
+
+    @Override
+    public List<Mutant> execute(String originalFilePath) {
+        List<Mutant> res = new ArrayList<>();
+        parse(originalFilePath);
+        List<IfStmt> ifStmts = cu.findAll(IfStmt.class);
+        int mutantNo = 0;
+        // 遍历所有的if语句
+        for (int i = 0; i < ifStmts.size(); i++) {
+            IfStmt ifStmt = ifStmts.get(i);
+            // 遍历if的condition中所有的方法调用
+            List<MethodCallExpr> methodCallExprs = ifStmt.getCondition().findAll(MethodCallExpr.class);
+            for (int j = 0; j < methodCallExprs.size(); j++) {
+                MethodCallExpr methodCallExpr = methodCallExprs.get(j);
+                boolean removeMethodCall = false;
+                try {
+                    ResolvedMethodDeclaration resolvedMethodDeclaration = methodCallExpr.resolve();
+                    String packageAndClassName = resolvedMethodDeclaration.getPackageName() + "." + resolvedMethodDeclaration.getClassName();
+                    // 判断是否是网络资源的if检查
+                    if (packageAndClassName.equals("java.net.Socket") || packageAndClassName.equals("java.net.ServerSocket")) {
+                        if (methodCallExpr.getName().asString().equals("isClosed") || methodCallExpr.getName().asString().equals("isBound")) {
+                            removeMethodCall = true;
+                        }
+                    }
+                    if (packageAndClassName.equals("ServerSocketChannel") || packageAndClassName.equals("NetworkChannel") || packageAndClassName.equals("SocketChannel")) {
+                        if (methodCallExpr.getName().asString().equals("isOpen")) {
+                            removeMethodCall = true;
+                        }
+                    }
+                    if (packageAndClassName.equals("ServerSocketChannel") && (methodCallExpr.getName().asString().equals("isConnected") || methodCallExpr.getName().asString().equals("isConnectionPending"))) {
+                        removeMethodCall = true;
+                    }
+                } catch (UnsolvedSymbolException e) {
+                    // logger.info("UnsolvedSymbolException in methodCallExpr - " + throwStmt.getExpression().asObjectCreationExpr().getType());
+                }
+                // 删除网络资源的if检查
+
+                if (removeMethodCall) {
+                    // 生成变异体
+                    // 在拷贝cu上删除close方法
+                    CompilationUnit cuCopy = generateCuCopy(originalFilePath);
+                    IfStmt ifStmtCopy = cuCopy.findAll(IfStmt.class).get(i);
+                    ifStmtCopy.setCondition(new BooleanLiteralExpr(true));
+
+                    // TODO 判断删除之后，if的condition是否为空，如果为空则删除整个if
+                    // 写入文件
+                    mutantNo++;
+                    String mutantName = FileUtil.getFileName(originalFilePath) + "_" + mutator + "_" + mutantNo + ".java";
+                    String mutantPath = new File("./mutants/").getAbsolutePath() + "/" + mutantName;
+                    logger.info("Generating mutant: " + mutantName);
+                    FileUtil.writeToFile(LexicalPreservingPrinter.print(cuCopy), mutantPath);
+                    // 生成变异体对象
+                    Mutant mutant = new Mutant(ifStmt.getRange().get().begin.line, mutator, originalFilePath, mutantPath);
+                    res.add(mutant);
+                }
+            }
+        }
+
+        return res;
+    }
+}

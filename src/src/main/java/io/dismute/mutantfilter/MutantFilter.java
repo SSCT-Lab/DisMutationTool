617e0ae740f9f94a1a2ed9b9f5e2d40d1ac25beb
dcZhao <zdc2022@outlook.com>
2025-05-28 12:32:40 +0800

    update

--- src/src/main/java/io/dismute/mutantfilter/MutantFilter.java
+++
@@ -1,9 +0,0 @@
-package io.dismute.mutantfilter;
-
-import io.dismute.mutantgen.Mutant;
-
-import java.util.List;
-
-public interface MutantFilter {
-    public List<Mutant> filter(List<Mutant> mutants);
-}

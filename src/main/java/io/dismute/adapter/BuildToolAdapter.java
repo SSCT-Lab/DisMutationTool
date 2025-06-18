cc608d66c524c1dc3c1380587179d3b02e61080a
Zheyuan Lin <zheyuanlin@smail.nju.edu.cn>
2025-06-18 11:04:47 +0800

    feat : add tool adapter module

---
+++ src/main/java/io/dismute/adapter/BuildToolAdapter.java
@@ -0,0 +1,78 @@
+package io.dismute.adapter;
+
+import io.dismute.mutantgen.Mutant;
+import io.dismute.singleton.Project;
+import io.dismute.singleton.PropertiesFile;
+import io.dismute.utils.CommandLineUtil;
+import io.dismute.utils.FileUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.file.Paths;
+
+public abstract class BuildToolAdapter {
+
+    private static final Logger logger = LogManager.getLogger(BuildToolAdapter.class);
+
+    protected static final int EXECUTION_TIMEOUT = Integer.parseInt(PropertiesFile.getInstance().getProperty("execution.timeout.seconds"));
+
+
+    // 生成classpath.txt文件，存储在输出目录下
+    public abstract void cleanAndGenerateClassPath();
+
+    //    void coverage();
+    public abstract int clean();
+    public abstract int compilation();
+    public abstract int cleanAndCompilation();
+
+
+    /**
+     * 增量编译某个变异体对应的.java文件
+     * 如果编译失败返回false
+     * 如果编译成功，编译结果存放在Project.mutantBytecodePath目录中，文件夹形式，文件夹名为变异体名，返回true
+     * @param mutant
+     * @return
+     */
+    public boolean incrementalCompilation(Mutant mutant) {
+        logger.info("IncrementalCompilation for mutant: {}", mutant.getMutatedName());
+        Project project = Project.getInstance();
+        String mutatedPath = mutant.getMutatedPath();
+        String originalName = mutant.getOriginalName();
+
+        // 在Project.mutantBytecodePath目录中创建以变异体名命名的文件夹
+        String compilationBasePath = Paths.get(project.getMutantBytecodePath(), FileUtil.getNameWithoutExtension(mutant.getMutatedPath())).toString();
+        FileUtil.createDirIfNotExist(compilationBasePath);
+
+        // 将变异体的.java文件复制到该文件夹中，并改回originalName
+        FileUtil.copyFileToTargetDir(mutatedPath, compilationBasePath, originalName);
+        String targetJavaFilePath = Paths.get(compilationBasePath, originalName).toString(); // 接下来要编译的.java文件，内容为变异后的，文件名为原始文件名
+        // 进入到在Project.mutantBytecodePath/<变异体名>目录下，对其中的变异体执行javac命令
+        /*
+        javac -d /Users/zhaodongchen/DismuteTestField/ \
+        -cp "/Users/zhaodongchen/code/distributedSources/apache-zookeeper-3.5.8/zookeeper-server/target/classes:$(cat /Users/zhaodongchen/code/distributedSources/apache-zookeeper-3.5.8/zookeeper-server/classpath.txt)" \
+        ServerCnxn.java
+        */
+        String classpath = FileUtil.readFileToString(project.getClasspathTxtPath());
+        if(StringUtils.isEmpty(classpath)) {
+            logger.error("Error while reading classpath.txt");
+            throw new RuntimeException("Error while reading classpath.txt");
+        }
+        String fullClasspath = project.getBuildOutputPath() + ":" + classpath;
+
+        int exitCode = CommandLineUtil.executeCommandAndRedirectOutputs(
+                compilationBasePath,
+                "javac",
+                "-g",
+                "-d", compilationBasePath,
+                "-cp", fullClasspath,
+                originalName
+        );
+        logger.info("Incremental compilation for mutant {} exited with code {}", mutant.getMutatedName(), exitCode);
+        return exitCode == 0;
+    }
+
+    // TODO 装载变异体交给engine
+    // 注：该方法不负责装载变异体
+    public abstract int testExecution(Mutant mutant, String... args);
+}
